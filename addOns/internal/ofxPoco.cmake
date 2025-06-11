338199c5fa0e727f930e927664e2a5661e976fbc
c-mendoza <c@matadata.com>
2025-06-11 13:57:18 -0400

    Fix ofxPoco

--- addOns/internal/ofxPoco.cmake
+++ addOns/internal/ofxPoco.cmake
@@ -8,7 +8,7 @@
 #
 # =================================================================
 
-set( NAME_ADDON     ofxPoco )       # <--- Set the name here
+set(NAME_ADDON ofxPoco)       # <--- Set the name here
 
 #==================================================================
 
@@ -16,16 +16,16 @@ set( NAME_ADDON     ofxPoco )       # <--- Set the name here
 # -----------------------------------------------------------------
 # ---------------------------- PATHS ------------------------------
 # -----------------------------------------------------------------
-set( PATH_SRC    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
-set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
+set(PATH_SRC ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src)
+set(PATH_LIBS ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs)
 
 # -----------------------------------------------------------------
 # ---------------------------- SOURCE -----------------------------
 # -----------------------------------------------------------------
 
-file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SRC}/*.cpp" )
-file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS}/*.cpp" )
-add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
+file(GLOB_RECURSE OFX_ADDON_CPP "${PATH_SRC}/*.cpp")
+file(GLOB_RECURSE OFX_ADDON_LIBS_CPP "${PATH_LIBS}/*.cpp")
+add_library(${NAME_ADDON} STATIC ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP})
 
 # -----------------------------------------------------------------
 # ---------------------------- HEADERS ----------------------------
@@ -36,8 +36,8 @@ add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
 #
 # OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS} )
 # include_directories( ${HEADERS_LIBS} )
-include_directories( "${PATH_LIBS}/openssl/include" )
-include_directories( "${PATH_LIBS}/poco/include" )
+include_directories("${PATH_LIBS}/openssl/include")
+include_directories("${PATH_LIBS}/poco/include")
 
 # -----------------------------------------------------------------
 # ------------------------------ LIBS  ----------------------------
@@ -48,34 +48,37 @@ include_directories( "${PATH_LIBS}/poco/include" )
 # --- Be sure to link them in target_link_libraries()
 # -----------------------------------------------------------------
 
-if( APPLE )
-	set(openSSL1 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/crypto.a" )
-	set(openSSL2 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/ssl.a" )
-
-	set( poco1    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/crypto.a" )
-    set( poco2    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/ssl.a" )
-    set( poco3    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoCrypto.a" )
-    set( poco4    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoFoundation.a" )
-    set( poco5    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoJSON.a" )
-    set( poco6    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNet.a" )
-    set( poco7    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNetSSL.a" )
-    set( poco8    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoXML.a" )
-    set( poco9    "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoZip.a" )
-#    find_library( CORE_FOUNDATION_LIB_${NAME_ADDON}  CoreFoundation)
-#    set( compiledLib    "${OF_CMAKE_ADDONS}/${NAME_ADDON}/lib/myLib.a" )
-   set(    EXTRA_LIBS_ofxPoco
-           ${poco1}
-		   ${poco2}
-		   ${poco3}
-		   ${poco4}
-		   ${poco5}
-		   ${poco6}
-		   ${poco7}
-		   ${poco8}
-		   ${poco9}
-		   ${openSSL1}
-		   ${openSSL2}
-           )
-endif()
-
-target_link_libraries( ofxPoco ${EXTRA_LIBS_ofxPoco} )
+if (APPLE)
+    set(openSSL1 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/crypto.a")
+    set(openSSL2 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/ssl.a")
+#
+#    set(poco1 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/crypto.a")
+#    set(poco2 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/openssl/lib/osx/ssl.a")
+#    set(poco3 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoCrypto.a")
+#    set(poco4 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoFoundation.a")
+#    set(poco5 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoJSON.a")
+#    set(poco6 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNet.a")
+#    set(poco7 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNetSSL.a")
+#    set(poco8 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoXML.a")
+#    set(poco9 "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoZip.a")
+    #    find_library( CORE_FOUNDATION_LIB_${NAME_ADDON}  CoreFoundation)
+    #    set( compiledLib    "${OF_CMAKE_ADDONS}/${NAME_ADDON}/lib/myLib.a" )
+    set(EXTRA_LIBS_ofxPoco
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoZip.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoCrypto.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoData.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoEncodings.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoFoundation.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoJSON.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoJWT.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNet.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoNetSSL.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoRedis.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoUtil.a"
+            "${OF_DIRECTORY_ABSOLUTE}/addons/ofxPoco/libs/poco/lib/osx/PocoXML.a"
+            ${openSSL1}
+            ${openSSL2}
+    )
+endif ()
+message(STATUS "${EXTRA_LIBS_ofxPoco}")
+target_link_libraries(ofxPoco ${EXTRA_LIBS_ofxPoco})
